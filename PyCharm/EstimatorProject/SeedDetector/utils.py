06ae00dd33996a442dd0a5a1c57e0e88dc510c07
iggy12345 <32805004+iggy12345@users.noreply.github.com>
2019-03-19 08:19:15 -0500

    added logging hook to train method

--- PyCharm/EstimatorProject/SeedDetector/utils.py
+++ PyCharm/EstimatorProject/SeedDetector/utils.py
@@ -3,7 +3,7 @@ import tensorflow as tf
 from .models import *
 
 
-def train(train_dict, test_dict, iterations, model_dir, batch_size):
+def train(train_dict, test_dict, iterations, model_dir, batch_size, logged_tensors=None):
     """Trains the nn for given number of iterations
     from the given model directory with a given batch_size"""
 
@@ -14,16 +14,16 @@ def train(train_dict, test_dict, iterations, model_dir, batch_size):
         train_input_fn = get_input_fn(train_dict["data"], train_dict["labels"], batch_size)
         eval_input_fn = get_input_fn(test_dict["data"], test_dict["labels"], batch_size)
 
-        estimator.train(input_fn=train_input_fn, steps=iterations)
+        estimator.train(input_fn=train_input_fn, steps=iterations, hooks=[logged_tensors])
 
         return estimator.evaluate(input_fn=eval_input_fn)
 
 
-def indefinite_train(train_dict, test_dict, batch_iterations, model_dir, batch_size):
+def indefinite_train(train_dict, test_dict, batch_iterations, model_dir, batch_size, logged_tensors=None):
 
     try:
         while True:
-            print(train(train_dict, test_dict, batch_iterations, model_dir, batch_size))
+            print(train(train_dict, test_dict, batch_iterations, model_dir, batch_size, logged_tensors))
     except Exception as e:
         # Catches the Ctrl+C break
         print("Finished training!")
