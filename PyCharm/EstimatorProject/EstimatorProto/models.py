1fdd23df015987a7fd6540dc2e0fcd1b10dc4218
iggy12345 <aaronjencks@gmail.com>
2019-03-20 08:11:03 -0500

    Renamed SeedDetector to EstimatorProto

---
+++ PyCharm/EstimatorProject/EstimatorProto/models.py
@@ -0,0 +1,75 @@
+import tensorflow as tf
+import tensorflow.estimator as te
+
+
+def cnn_model_fn(features, labels, mode, params):
+    """Model function for CNN."""
+    # Input Layer
+    input_layer = tf.reshape(features, [-1, params["height"], params["width"], 1])
+
+    # Convolutional Layer #1
+    conv1 = tf.keras.layers.Conv2D(
+        filters=32,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)(input_layer)
+
+    # Pooling Layer #1
+    pool1 = tf.keras.layers.MaxPooling2D(pool_size=[2, 2], strides=2)(conv1)
+
+    # Convolutional Layer #2 and Pooling Layer #2
+    conv2 = tf.keras.layers.Conv2D(
+        filters=64,
+        kernel_size=[5, 5],
+        padding="same",
+        activation=tf.nn.relu)(pool1)
+    pool2 = tf.keras.layers.MaxPooling2D(pool_size=[2, 2], strides=2)(conv2)
+
+    # Dense Layer
+    pool2_flat = tf.reshape(pool2, [-1, 7 * 7 * 64])
+    dense = tf.keras.layers.Dense(units=1024, activation=tf.nn.relu)(pool2_flat)
+    dropout = tf.keras.layers.Dropout(rate=0.4)(
+        dense, training=mode == tf.estimator.ModeKeys.TRAIN)
+
+    # Logits Layer
+    logits = tf.keras.layers.Dense(units=10)(dropout)
+
+    predictions = {
+        # Generate predictions (for PREDICT and EVAL mode)
+        "classes": tf.argmax(input=logits, axis=1),
+        # Add `softmax_tensor` to the graph. It is used for PREDICT and by the
+        # `logging_hook`.
+        "probabilities": tf.nn.softmax(logits, name="softmax_tensor")
+    }
+
+    if mode == tf.estimator.ModeKeys.PREDICT:
+        return tf.estimator.EstimatorSpec(mode=mode, predictions=predictions)
+
+    # Calculate Loss (for both TRAIN and EVAL modes)
+    loss = tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
+
+    # Configure the Training Op (for TRAIN mode)
+    if mode == tf.estimator.ModeKeys.TRAIN:
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=0.001)
+        train_op = optimizer.minimize(
+            loss=loss,
+            global_step=tf.train.get_global_step())
+        return tf.estimator.EstimatorSpec(mode=mode, loss=loss, train_op=train_op)
+
+    # Add evaluation metrics (for EVAL mode)
+    eval_metric_ops = {
+        "accuracy": tf.metrics.accuracy(
+            labels=labels, predictions=predictions["classes"])
+    }
+    return tf.estimator.EstimatorSpec(
+        mode=mode, loss=loss, eval_metric_ops=eval_metric_ops)
+
+
+def generate_model(model_dir, image_size):
+    """Generates a new tensorflow model using estimators"""
+
+    return te.Estimator(
+        model_fn=cnn_model_fn,
+        model_dir=model_dir,
+        params={"height": image_size[0], "width": image_size[1]}
+    )
