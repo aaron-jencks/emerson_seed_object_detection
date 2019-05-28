Fixed image rotation and plotting opacity/speed

PyCharm/pmd_implementation/pmd_implementation/main.py
@@ -4,7 +4,7 @@ from pmd_implementation.display_util.daemons import DisplayMachine
 from pmd_implementation.display_util.display_util import *
 from pmd_implementation.cam_util.daemons import CamCtrl
 from pmd_implementation.data_util.daemons import DataDaemon
-from pmd_implementation.dependencies.display_util.string_display_util import print_notification
+from pmd_implementation.dependencies.display_util.string_display_util import print_notification, print_warning
 
 from PyQt5.QtCore import Qt, QThread
 from PyQt5.QtWidgets import QApplication, QSlider
@@ -15,12 +15,14 @@ import numpy as np
 
 import pyximport
 pyximport.install(setup_args={"include_dirs": np.get_include()})
+import pmd_implementation.data_util.cy_scatter as ct
 
 
 class DepthAverager(QThread, StateMachine):
     """Manages the depth averaging for this application"""
 
-    slider_max = 300
+    slider_scale = 1000
+    slider_max = 3
     slider_min = 0
 
     def __init__(self, disp_daemon: DisplayMachine):
@@ -34,6 +36,7 @@ class DepthAverager(QThread, StateMachine):
         self.disp_ctrl_update = disp_daemon.update
         self.disp_ctrl = disp_daemon
         self.scatter = None
+        self.gray = None
         self.set_status('Loading...')
         self.init_widget()
 
@@ -59,20 +62,58 @@ class DepthAverager(QThread, StateMachine):
 
         # Max bar
         self.disp_ctrl.widgets['cmap_max'].setValue(self.data_proc.cmap_up)
-        self.disp_ctrl.widgets['cmap_max'].valueChanged.connect(lambda v: self.update_slider(v, 'up'))
+
+        self.disp_ctrl.widgets['cmap_max'].valueChanged.connect(lambda v:
+                                                                self.disp_ctrl.widgets['cmap_max_lbl_val'].setText(
+                                                                    '{} m'.format(round(v / self.slider_scale, 1))))
+
+        self.disp_ctrl.widgets['cmap_max'].sliderReleased.connect(lambda: self.update_slider(
+            self.disp_ctrl.widgets['cmap_max'].value(), 'up'))
 
         # Mid bar
         self.disp_ctrl.widgets['cmap_mid'].setValue(self.data_proc.cmap_mid)
-        self.disp_ctrl.widgets['cmap_mid'].valueChanged.connect(lambda v: self.update_slider(v, 'mid'))
+
+        self.disp_ctrl.widgets['cmap_mid'].valueChanged.connect(lambda v:
+                                                                self.disp_ctrl.widgets['cmap_mid_lbl_val'].setText(
+                                                                    '{} m'.format(round(v / self.slider_scale, 1))))
+
+        self.disp_ctrl.widgets['cmap_mid'].sliderReleased.connect(lambda: self.update_slider(
+            self.disp_ctrl.widgets['cmap_mid'].value(), 'mid'))
 
         # Min bar
         self.disp_ctrl.widgets['cmap_min'].setValue(self.data_proc.cmap_low)
-        self.disp_ctrl.widgets['cmap_min'].valueChanged.connect(lambda v: self.update_slider(v, 'low'))
+
+        self.disp_ctrl.widgets['cmap_min'].valueChanged.connect(lambda v:
+                                                                self.disp_ctrl.widgets['cmap_min_lbl_val'].setText(
+                                                                    '{} m'.format(round(v / self.slider_scale, 1))))
+
+        self.disp_ctrl.widgets['cmap_min'].sliderReleased.connect(lambda: self.update_slider(
+            self.disp_ctrl.widgets['cmap_min'].value(), 'low'))
 
         # Sets the limits of the bar
-        self.disp_ctrl.widgets['cmap_max'].setRange(self.slider_min, self.slider_max)
-        self.disp_ctrl.widgets['cmap_mid'].setRange(self.slider_min, self.slider_max)
-        self.disp_ctrl.widgets['cmap_min'].setRange(self.slider_min, self.slider_max)
+        self.disp_ctrl.widgets['cmap_max'].setRange(self.slider_min * self.slider_scale,
+                                                    self.slider_max * self.slider_scale)
+
+        self.disp_ctrl.widgets['cmap_mid'].setRange(self.slider_min * self.slider_scale,
+                                                    self.slider_max * self.slider_scale)
+
+        self.disp_ctrl.widgets['cmap_min'].setRange(self.slider_min * self.slider_scale,
+                                                    self.slider_max * self.slider_scale)
+
+        # region Handles the Depth Scale bar
+
+        self.disp_ctrl.widgets['dmp_scale'].setRange(1, 500)
+        self.disp_ctrl.widgets['dmp_scale'].setValue(1)
+
+        self.disp_ctrl.widgets['dmp_scale'].valueChanged.connect(lambda v:
+                                                                 self.disp_ctrl.widgets['dmp_scale_lbl'].setText(
+                                                                     'Depth Scale: {} x'.format(
+                                                                         round(v, 1))))
+
+        self.disp_ctrl.widgets['dmp_scale'].sliderReleased.connect(lambda: self.update_slider(
+            self.disp_ctrl.widgets['dmp_scale'].value(), 'scale'))
+
+        # endregion
 
         # Initializes the sliders to the values of the data processor
         self.init_slider()
@@ -91,6 +132,10 @@ class DepthAverager(QThread, StateMachine):
 
     def plot_data(self, msg: JMsg):
         """Plots the frame data onto the window."""
+
+        if msg is None:
+            print_warning('Trying to display empty frame!')
+            return
 
         img = msg.data[0]
         depth = msg.data[1]
@@ -115,17 +160,16 @@ class DepthAverager(QThread, StateMachine):
 
         img_disp.setImage(img)
 
+        # self.gray = self.disp_ctrl.widgets['img'].getImageItem()
+        # self.gray.rotate(90, 320, 240, 0, local=True)
+
     def plot_cloud(self, data, colors):
         """Plots the pointcloud onto the window."""
+        # dimg = ct.create_color_depth_image(data, colors, self.cam_ctrl.height, self.cam_ctrl.width)
+        # self.disp_ctrl.widgets['dimg'].setImage(dimg)
 
-        # data = np.array([(a, b, c) for a, b, c in zip(x, y, z)])
-        # print(data.size)
+        data = ct.apply_depth_scale(data, self.data_proc.scale)
         self.scatter.setData(pos=data, color=colors)
-
-        # Sets the center of the camera to the center of the cloud
-        # self.disp_ctrl.widgets['dmp'].pan(self.data_proc.center_x,
-        #                                   self.data_proc.center_y,
-        #                                   self.data_proc.center_z)
 
     def init_camera(self):
         """Initializes the camera connecting to one if possible,
@@ -156,22 +200,34 @@ class DepthAverager(QThread, StateMachine):
         # region Sets up the colormap sliders
 
         self.disp_ctrl_update.emit(JMsg('create_label',
-                                   get_text_widg_dict('Max', 'cmap_max_lbl', 0, 2)))
+                                   get_text_widg_dict('Max', 'cmap_max_lbl', 2, 1)))
         self.disp_ctrl_update.emit(JMsg('create_slider',
-                                   get_slider_widg_dict(Qt.Vertical, 'cmap_max', 1, 2,
-                                                        'Adjusts the upper saturation limit of the colormap')))
+                                   get_slider_widg_dict(Qt.Vertical, 'cmap_max', 3, 1,
+                                                        tip='Adjusts the upper saturation limit of the colormap')))
+        self.disp_ctrl_update.emit(JMsg('create_label',
+                                        get_text_widg_dict('3.0 m', 'cmap_max_lbl_val', 4, 1)))
 
         self.disp_ctrl_update.emit(JMsg('create_label',
-                                   get_text_widg_dict('Mid', 'cmap_mid_lbl', 0, 3)))
+                                   get_text_widg_dict('Mid', 'cmap_mid_lbl', 2, 2)))
         self.disp_ctrl_update.emit(JMsg('create_slider',
-                                   get_slider_widg_dict(Qt.Vertical, 'cmap_mid', 1, 3,
-                                                        'Adjusts the mid-point of the colormap')))
+                                   get_slider_widg_dict(Qt.Vertical, 'cmap_mid', 3, 2,
+                                                        tip='Adjusts the mid-point of the colormap')))
+        self.disp_ctrl_update.emit(JMsg('create_label',
+                                        get_text_widg_dict('1.5 m', 'cmap_mid_lbl_val', 4, 2)))
 
         self.disp_ctrl_update.emit(JMsg('create_label',
-                                   get_text_widg_dict('Min', 'cmap_min_lbl', 0, 4)))
+                                   get_text_widg_dict('Min', 'cmap_min_lbl', 2, 3)))
         self.disp_ctrl_update.emit(JMsg('create_slider',
-                                   get_slider_widg_dict(Qt.Vertical, 'cmap_min', 1, 4,
-                                                        'Adjusts the lower saturation limit of the colormap')))
+                                   get_slider_widg_dict(Qt.Vertical, 'cmap_min', 3, 3,
+                                                        tip='Adjusts the lower saturation limit of the colormap')))
+        self.disp_ctrl_update.emit(JMsg('create_label',
+                                        get_text_widg_dict('0.0 m', 'cmap_min_lbl_val', 4, 3)))
+
+        self.disp_ctrl_update.emit(JMsg('create_label',
+                                        get_text_widg_dict('Depth Scale: 1.0 m', 'dmp_scale_lbl', 5, 0)))
+        self.disp_ctrl.widgets['dmp_scale_lbl'].setAlignment(Qt.AlignLeft)
+        self.disp_ctrl_update.emit(JMsg('create_slider',
+                                        get_slider_widg_dict(Qt.Horizontal, 'dmp_scale', 4, 0)))
 
         # endregion
 
@@ -182,41 +238,58 @@ class DepthAverager(QThread, StateMachine):
         self.disp_ctrl_update.emit(JMsg('create_widget',
                                    get_custom_widg_dict(pg.ImageView, 'img', 1, 0)))
 
-        self.disp_ctrl_update.emit(JMsg('create_label',
-                                   get_text_widg_dict('Point Cloud', 'dmp_lbl', 0, 1)))
-        self.disp_ctrl_update.emit(JMsg('create_widget',
-                                   get_custom_widg_dict(pgl.GLViewWidget, 'dmp', 1, 1)))
+        # self.disp_ctrl_update.emit(JMsg('create_label',
+        #                                 get_text_widg_dict('Depth Image', 'dimg_lbl', 0, 1)))
+        # args = get_custom_widg_dict(pg.ImageView, 'dimg', 1, 1, 1, 3)
+        # self.disp_ctrl_update.emit(JMsg('create_widget',
+        #                                 args))
 
-        self.scatter = pgl.GLScatterPlotItem(pos=np.zeros(shape=(307200, 3), dtype=float))
+        self.disp_ctrl_update.emit(JMsg('create_label',
+                                   get_text_widg_dict('Point Cloud', 'dmp_lbl', 2, 0)))
+        self.disp_ctrl_update.emit(JMsg('create_widget',
+                                   get_custom_widg_dict(pgl.GLViewWidget, 'dmp', 3, 0)))
+
+        self.scatter = pgl.GLScatterPlotItem(pos=np.zeros(shape=(307200, 3), dtype=float), size=1)
         self.disp_ctrl.widgets['dmp'].addItem(self.scatter)
+
+        # region view formatting
+
+        self.scatter.rotate(180, 0, 0, 0)
+        self.scatter.setGLOptions('opaque')
         widg = self.disp_ctrl.widgets['dmp']
-        widg.pan(240, 320, 40)
-        # TODO Pan camera here to center of image location
+        # widg.mouseReleaseEvent = lambda v: self.update_center_display()
+        widg.pan(-320, -240, 0)
+        widg.setCameraPosition(distance=1746, elevation=43, azimuth=-479)
 
         # endregion
 
+        # endregion
+
+    # def update_center_display(self):
+    #     opts = self.disp_ctrl.widgets['dmp'].opts
+    #     self.set_status("Ready pos: {}, {}, {}".format(opts['distance'], opts['elevation'], opts['azimuth']))
+
     def init_slider(self):
-        self.disp_ctrl.widgets['cmap_max'].setValue(self.data_proc.cmap_up)
-        self.disp_ctrl.widgets['cmap_mid'].setValue(self.data_proc.cmap_mid)
-        self.disp_ctrl.widgets['cmap_min'].setValue(self.data_proc.cmap_low)
+        self.disp_ctrl.widgets['cmap_max'].setValue(self.data_proc.cmap_up * self.slider_scale)
+        self.disp_ctrl.widgets['cmap_mid'].setValue(self.data_proc.cmap_mid * self.slider_scale)
+        self.disp_ctrl.widgets['cmap_min'].setValue(self.data_proc.cmap_low * self.slider_scale)
 
     def update_slider(self, value: int, slider: str):
         """Updates the value of the adjusted slider"""
-        self.data_proc.rx.emit(JMsg(slider, value / 100))
+
+        if slider != 'scale':
+            value /= self.slider_scale
+
+        self.set_status('Updating slider bars')
+        self.data_proc.cmap_up_to_date = False
+        self.data_proc.rx.emit(JMsg(slider, value))
 
         # Waits for the cmap value to be updated
-        while True:
-            if slider == 'up':
-                cm = self.data_proc.cmap_up
-            elif slider == 'mid':
-                cm = self.data_proc.cmap_mid
-            else:
-                cm = self.data_proc.cmap_low
-
-            if cm == value:
-                break
+        while not self.data_proc.cmap_up_to_date:
+            pass
 
         self.set_slider_limits()
+        self.reset_status()
 
     def set_slider_limits(self):
         """Updates the limits of the slider values so that they don't exceed each other."""
@@ -240,6 +313,9 @@ class DepthAverager(QThread, StateMachine):
     def set_status(self, message: str):
         """Sets the status bar of the main display"""
         self.disp_ctrl_update.emit(JMsg('status', message))
+
+    def reset_status(self):
+        self.set_status('Ready')
 
 
 if __name__ == "__main__":