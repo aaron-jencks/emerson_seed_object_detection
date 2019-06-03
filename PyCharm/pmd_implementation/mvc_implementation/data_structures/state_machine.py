29cad14306fe2d244563e2daf80ff7cc37042764
iggy12345 <aaronjencks@gmail.com>
2019-06-03 13:58:41 -0500

    Added StateMachineManager support

--- PyCharm/pmd_implementation/mvc_implementation/data_structures/state_machine.py
+++ PyCharm/pmd_implementation/mvc_implementation/data_structures/state_machine.py
@@ -6,8 +6,11 @@ import threading
 from PyQt5.QtCore import pyqtSignal, QObject, QThread
 
 from collections import deque
+from mvc_implementation.data_structures.Q import DequeManager, DictProxy
 
 from mvc_implementation.dependencies.display_util.string_display_util import print_warning
+
+from queue import Queue
 
 from colorama import Fore
 
@@ -23,22 +26,20 @@ class JMsg:
         return "{}: {}".format(self.message, str(self.data))
 
 
-class StateMachine(QObject):
-    """Represents a state machine that can be ran on another thread."""
+class StateManagerModule(threading.Thread):
+    """Handles the queueing and unqueueing of events for the state machines, in a non-blocking way."""
 
-    def __init__(self, auto_start: bool = False, **kwargs):
-        super().__init__(**kwargs)
+    next_state = pyqtSignal(str, object)
 
+    def __init__(self, state_queue: Queue, states: DictProxy, auto_start: bool = True):
+        super().__init__()
+
+        self.state_queue = state_queue if state_queue is not None else Queue
         self.isStopping = False
-        self.state_queue = deque()
-        self.states = {'init': self.initial_state, 'STOP': self.final_state, 'idle': self.idle_state}
-        self.state_queue.append(JMsg('init'))
-
-        # Used to transfer JMsg data to individual states
-        self.data = None
+        self.states = {} if states is None else states
 
         if auto_start:
-            self.run()
+            self.start()
 
     def run(self):
         self.isStopping = False
@@ -50,46 +51,76 @@ class StateMachine(QObject):
                 meth, data = state
 
                 # Runs the selected state
-                try:
-                    if data is not None:
-                        meth(data)
-                    else:
-                        meth()
-                except Exception as e:
-                    et, ev, tb = sys.exc_info()
-                    exc = "Exception was thrown: {}\n".format(e)
-                    for l in traceback.format_exception(et, ev, tb):
-                        exc += l
-                    exc += '\nExitting...'
-                    print_warning(exc)
-                    self.stop()
+                self.next_state.emit(meth, data)
 
-    def stop(self) -> None:
-        """Flags the thread to stop running."""
+    def stop(self):
         self.isStopping = True
 
     def get_next_state(self):
         """Collects the next state from the state_queue, if it doesn't exist, or the queue is empty, goes to idle."""
-        if len(self.state_queue) == 0:
-            return self.states['idle']
+        if self.state_queue.qsize() == 0:
+            return 'idle', None
         else:
-            msg, data = self.state_queue.popleft()
+            msg, data = self.state_queue.get()
             if msg in self.states:
-                return self.states[msg], data
+                return msg, data
+
+
+class StateMachine(QObject):
+    """Represents a state machine that can be ran on another thread."""
+
+    def __init__(self, state_manager: DequeManager = None, auto_start: bool = False, **kwargs):
+        super().__init__(**kwargs)
+
+        self.isStopping = False
+        self.state_queue = Queue()
+        self.state_reg_mgr = state_manager if state_manager is not None else DequeManager()
+        self.states = self.state_reg_mgr.DictProxy({
+            'init': self.initial_state, 'STOP': self.final_state, 'idle': self.idle_state})
+        self.append_states(['init'])
+
+        self.state_exec_mgr = StateManagerModule(self.state_queue, self.states, False)
+        self.state_exec_mgr.next_state.connect(self.exec_state)
+
+        if auto_start:
+            self.run()
+
+    def exec_state(self, meth, data):
+        # Runs the selected state
+        try:
+            if data is not None:
+                self.states[meth](data)
+            else:
+                self.states[meth]()
+        except Exception as e:
+            et, ev, tb = sys.exc_info()
+            exc = "Exception was thrown: {}\n".format(e)
+            for l in traceback.format_exception(et, ev, tb):
+                exc += l
+            exc += '\nExitting...'
+            print_warning(exc)
+            self.stop()
+
+    def run(self):
+        self.state_exec_mgr.start()  # Starts the state event loop
+
+    def stop(self) -> None:
+        """Flags the thread to stop running."""
+        self.append_states(['STOP'])
 
     def append_states(self, states: list):
         for state in states:
             if isinstance(state, str):
-                self.state_queue.append((state, None))
+                self.state_queue.put((state, None))
                 continue
 
             try:
                 iter(state)
-                self.state_queue.append(state)
+                self.state_queue.put(state)
                 continue
             except TypeError:
                 if isinstance(state, JMsg):
-                    self.state_queue.append((state.message, state.data))
+                    self.state_queue.put((state.message, state.data))
                     continue
                 else:
                     raise TypeError('Type {} cannot be enqueued'.format(type(state)))
@@ -108,6 +139,7 @@ class StateMachine(QObject):
     def final_state(self):
         """This is the final state executed by the state machine."""
         self.isStopping = True
+        self.state_exec_mgr.stop()
 
     # endregion
 
@@ -124,24 +156,24 @@ class SocketedStateMachine(StateMachine):
         self.rx.connect(self.__recieve_msg)
 
     def __recieve_msg(self, msg: JMsg):
-        self.state_queue.append((msg.message, msg.data))
+        self.append_states([msg])
 
 
-class ThreadedStateMachine(QThread, StateMachine):
+class ThreadedStateMachine(StateMachine, threading.Thread):
     """Represents a state machine that can be ran on another thread."""
 
     def __init__(self, parent=None, auto_start: bool = False, **kwargs):
-        super().__init__()  # auto_start)
-        StateMachine.__init__(self, auto_start)
+        super().__init__(auto_start=auto_start)
+        # StateMachine.__init__(self, auto_start)
 
-        self.isStopping = False
-        self.state_queue = deque()
-        self.states = {'init': self.initial_state, 'STOP': self.final_state, 'idle': self.idle_state}
-        self.state_queue.append('init')
+        # self.isStopping = False
+        # self.state_queue = deque()
+        # self.states = {'init': self.initial_state, 'STOP': self.final_state, 'idle': self.idle_state}
+        # self.state_queue.append('init')
         self.parent = parent
 
         # Used to transfer JMsg data to individual states
-        self.data = None
+        # self.data = None
 
         if auto_start:
             self.run()
@@ -164,4 +196,4 @@ class ThreadedSocketedStateMachine(ThreadedStateMachine):
         self.rx.connect(self.__recieve_msg)
 
     def __recieve_msg(self, msg: JMsg):
-        self.state_queue.append((msg.message, msg.data))
+        self.append_states([msg])
