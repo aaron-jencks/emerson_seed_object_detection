449d2f1dac395beffdf9f0420811e36404aa0faf
Aaron Jencks <32805004+iggy12345@users.noreply.github.com>
2019-07-16 19:18:49 -0500

    Fixed the incorrect depth values, again...

--- PyCharm/pmd_implementation/labview_recorder/run_me.bat
+++ PyCharm/pmd_implementation/labview_recorder/run_me.bat
@@ -1,4 +1,4 @@
 @echo off
 rem cd /d "C:\Users\aaron.jencks\Documents\GitHub\emerson_seed_object_detection\PyCharm\pmd_implementation\labview_recorder"
-python setup.py build_ext
+python setup.py build_ext --inplace
 python main.py
